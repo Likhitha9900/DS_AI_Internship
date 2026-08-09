ed2f5fbb20eef5a8804cdc8ba351dbf20a2a6598
Likhitha9900 <likhithavidyanidhi@gmail.com>
2026-08-10 01:02:22 +0530

    8 commit

---
+++ day5/exm2.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+x = np.array([[2, 1, 1],
+              [2, 1, 3]])
+
+y = np.reshape(x, (3, 2))
+
+print(y)
---
+++ day5/test.py
@@ -0,0 +1,5 @@
+marks=[10,28,40,35]
+new_marks=[]
+for x in marks:
+    new_marks.append(x+5)
+    print(new_marks)
